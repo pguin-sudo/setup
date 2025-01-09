79fa03d95efb975c50638f9e6187d71e32e8ca62
pguin-sudo <138515193+pguin-sudo@users.noreply.github.com>
2025-01-10 00:12:06 +0500

    Change shell script to python

---
+++ setup.py
@@ -0,0 +1,30 @@
+import subprocess
+
+
+def do_or_skip(prompt, command) -> bool:
+    answer = input(f"{prompt} (Y/n)? ").strip().lower()
+    if answer in ("", "y"):
+        return subprocess.run(command, shell=True).returncode == 1
+    else:
+        print("Ok, skipping...")
+        return False
+
+
+print("<|==============|          Applying config           |==============|>")
+do_or_skip("Processing?", "cp -r $(pwd)/.config ~/")
+print("Config has been copied successfully")
+
+print("<|==============| Packages that are included in HyDE |==============|>")
+do_or_skip("Processing?", "yay -S eza fish starship")
+
+print("<|==============|           Fixes for fish           |==============|>")
+do_or_skip("Processing?", "yay -S noto-fonts-emoji")
+
+print("<|==============|      Very important packages       |==============|>")
+do_or_skip("Processing?", "yay -S helix htop")
+
+print("<|==============|           Others packages          |==============|>")
+do_or_skip(
+    "Processing?",
+    "yay -S telegram-desktop cava krita libreoffice steam obs-studio byedpi-bin",
+)
