842e95e953199a7912e0171c34fb47b506dfa607
PGuin <138515193+pguin-sudo@users.noreply.github.com>
2025-01-25 12:02:55 +0500

    Split commands in setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 import subprocess
 
 
-def do_or_skip(prompt, command) -> bool:
+def do_or_skip(prompt, *commands):
     answer = input(f"\033[34m::\033[0m {prompt} [Y/n]? ").strip().lower()
     if answer in ("", "y"):
-        return subprocess.run(command, shell=True).returncode == 1
+        for command in commands:
+            subprocess.run(command, shell=True)
     else:
         print("Ok, skipping...")
-        return False
 
 
 print("<|==============|          Applying config           |==============|>")
@@ -15,28 +15,33 @@ do_or_skip("Processing?", "cp -vri $(pwd)/.config ~/ ")
 print("Config has been copied successfully")
 
 print("<|==============| Packages that are included in HyDE |==============|>")
-do_or_skip("Processing?", "yay -S eza fish starship")
+do_or_skip("Processing?", "yay -S eza", "yay -S fish", "yay -S starship")
 
 print("<|==============|           Fixes for fish           |==============|>")
 do_or_skip("Processing?", "yay -S noto-fonts-emoji")
 
 print("<|==============|      Very important packages       |==============|>")
-do_or_skip("Processing?", "yay -S helix htop")
+do_or_skip("Processing?", "yay -S helix", "yay -S htop")
 
 print("<|==============|           Others packages          |==============|>")
 do_or_skip(
     "Processing?",
-    "yay -S telegram-desktop cava krita libreoffice steam obs-studio byedpi-bin",
+    "yay -S telegram-desktop",
+    "yay -S cava",
+    "yay -S krita",
+    "yay -S libreoffice",
+    "yay -S steam",
+    "yay -S obs-studio",
+    "yay -S byedpi-bin",
 )
 
 print("<|==============|     Setup notepad environment      |==============|>")
 do_or_skip(
     "Processing?",
-    (
-        "cp -vr $(pwd)/Notepad ~/ &&"
-        "yay -S cron &&"
-        "(crontab -l 2>/dev/null; echo '*/5 * * * * cd ~/Documents/Notepad && ./sync.sh') | crontab -"
-    ),
+    "cp -vr $(pwd)/Notepad ~/",
+    "yay -S cron",
+    "(crontab -l 2>/dev/null; echo '*/5 * * * * cd ~/Documents/Notepad",
+    "./sync.sh') | crontab -",
 )
 
 print("<|==============|            That's all...           |==============|>")
