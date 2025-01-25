ac5f943fb1b9f8724bfb16ba9352ed32f26d68d1
PGuin <138515193+pguin-sudo@users.noreply.github.com>
2025-01-25 17:30:34 +0500

    Add proxy

--- setup.py
+++ setup.py
@@ -1,12 +1,31 @@
 import subprocess
+import requests
 
 
 # FUNCTIONS
 
 
+class bcolors:
+    HEADER = "\033[95m"
+    OKBLUE = "\033[94m"
+    OKCYAN = "\033[96m"
+    OKGREEN = "\033[92m"
+    WARNING = "\033[93m"
+    FAIL = "\033[91m"
+    ENDC = "\033[0m"
+    BOLD = "\033[1m"
+    UNDERLINE = "\033[4m"
+
+
+class formatting:
+    HEADER = "<|==============|"
+    HEADER_END = "|==============|>"
+    WARNING = "[WARNING] "
+
+
 def ask(prompt):
     """Ask user for a yes/no input and return True for 'yes' and False for 'no'."""
-    answer = input(f"\033[34m::\033[0m {prompt} [Y/n]? ").strip().lower()
+    answer = input(f"{bcolors.OKBLUE}::{bcolors.ENDC} {prompt} [Y/n]? ").strip().lower()
     return answer in ("", "y")
 
 
@@ -21,7 +40,9 @@ def execute_commands(*commands):
 
 def process_config():
     """Process the configuration setup."""
-    print("<|==============|          Applying config           |==============|>")
+    print(
+        f"{bcolors.HEADER}{formatting.HEADER}          Applying config           {formatting.HEADER_END}{bcolors.ENDC}"
+    )
     if ask("Processing?"):
         execute_commands("cp -vri $(pwd)/config/* ~/.config/ ")
         print("Config has been copied successfully")
@@ -29,28 +50,36 @@ def process_config():
 
 def install_hyde_packages():
     """Install HyDE related packages."""
-    print("<|==============| Packages that are included in HyDE |==============|>")
+    print(
+        f"{bcolors.HEADER}{formatting.HEADER} Packages that are included in HyDE {formatting.HEADER_END}{bcolors.ENDC}"
+    )
     if ask("Processing?"):
         execute_commands("yay -S eza", "yay -S fish", "yay -S starship")
 
 
 def install_fish_fixes():
     """Install fixes for fish."""
-    print("<|==============|           Fixes for fish           |==============|>")
+    print(
+        f"{bcolors.HEADER}{formatting.HEADER}           Fixes for fish           {formatting.HEADER_END}{bcolors.ENDC}"
+    )
     if ask("Processing?"):
         execute_commands("yay -S noto-fonts-emoji")
 
 
 def install_important_packages():
     """Install important packages."""
-    print("<|==============|      Very important packages       |==============|>")
+    print(
+        f"{bcolors.HEADER}{formatting.HEADER}      Very important packages       {formatting.HEADER_END}{bcolors.ENDC}"
+    )
     if ask("Processing?"):
         execute_commands("yay -S helix", "yay -S htop")
 
 
 def install_other_packages():
     """Install other packages."""
-    print("<|==============|           Others packages          |==============|>")
+    print(
+        f"{bcolors.HEADER}{formatting.HEADER}           Others packages          {formatting.HEADER_END}{bcolors.ENDC}"
+    )
     if ask("Processing?"):
         execute_commands(
             "yay -S telegram-desktop",
@@ -63,9 +92,77 @@ def install_other_packages():
         )
 
 
+def setup_zapret_and_dns_proxy():
+    """Setup zapret and DNS proxy."""
+    print(
+        f"{bcolors.HEADER}{formatting.HEADER}     Setup zapret and dns proxy     {formatting.HEADER_END}{bcolors.ENDC}"
+    )
+    if ask("Processing?"):
+        url = "https://api.github.com/repos/bol-van/zapret/releases/latest"
+        response = requests.get(url)
+        if response.status_code == 200:
+            release_data = response.json()
+            download_url = next(
+                (
+                    asset["browser_download_url"]
+                    for asset in release_data["assets"]
+                    if ".tar.gz" in asset["name"] and "openwrt" not in asset["name"]
+                ),
+                None,
+            )
+            if download_url:
+                print(f"Downloading from {download_url}")
+                tar_file = download_url.split("/")[-1]
+
+                execute_commands(f"sudo rm -f ./{tar_file}")
+
+                execute_commands(f"sudo wget {download_url}")
+
+                print(f"Extracting {tar_file} ({tar_file[:-7]})")
+                execute_commands(
+                    "sudo mkdir /opt/zapret",
+                    f"sudo tar -xzvf ./{tar_file}",
+                    f"sudo cp -r $(pwd)/{tar_file[:-7]}/* /opt/zapret",
+                    f"sudo rm -rf $(pwd)/{tar_file[:-7]}",
+                    f"sudo rm $(pwd)/{tar_file}",
+                )
+            else:
+                print("No suitable tar.gz file found in the release assets.")
+        else:
+            print(f"Failed to fetch release info: {response.status_code}")
+
+        execute_commands(
+            "sudo /opt/zapret/install_bin.sh",
+            "sudo /opt/zapret/install_prereq.sh",
+            "yay -S dnscrypt-proxy",
+            "sudo cp $(pwd)/etc/dnscrypt-proxy/dnscrypt-proxy.toml /etc/dnscrypt-proxy/dnscrypt-proxy.toml",
+            "sudo chattr -i /etc/resolv.conf",
+            "sudo cp $(pwd)/etc/resolv.conf /etc/resolv.conf",
+            "sudo chattr +i /etc/resolv.conf",
+            "sudo systemctl enable dnscrypt-proxy.service",
+            "sudo systemctl start dnscrypt-proxy.service",
+            "sudo systemctl enable systemd-resolved.service",
+            "sudo systemctl start systemd-resolved.service",
+        )
+        print(
+            f"{bcolors.WARNING}{formatting.WARNING}IF YOU DO NOT HAVE WORKING STRATEGY YOU WILL NOT ABLE TO SETUP CONFIG IN ZAPRET{bcolors.ENDC}"
+        )
+        if ask("Run blockcheck.sh for searching strategy?"):
+            execute_commands(
+                "sudo /opt/zapret/blockcheck.sh",
+            )
+        if ask("Run discord fix?"):
+            execute_commands(
+                "sudo cp /opt/zapret/init.d/custom.d.examples.linux/50-discord /opt/zapret/init.d/sysv/custom.d/50-discord"
+            )
+        execute_commands("sudo /opt/zapret/install_easy.sh")
+
+
 def setup_notepad_environment():
     """Setup the notepad environment."""
-    print("<|==============|     Setup notepad environment      |==============|>")
+    print(
+        f"{bcolors.HEADER}{formatting.HEADER}     Setup notepad environment      {formatting.HEADER_END}{bcolors.ENDC}"
+    )
     if ask("Processing?"):
         execute_commands(
             "cp -vr $(pwd)/Notepad ~/",
@@ -81,8 +178,11 @@ def main():
     install_fish_fixes()
     install_important_packages()
     install_other_packages()
+    setup_zapret_and_dns_proxy()
     setup_notepad_environment()
-    print("<|==============|            That's all...           |==============|>")
+    print(
+        f"{bcolors.HEADER}{formatting.HEADER}            That's all...           {formatting.HEADER_END}{bcolors.ENDC}"
+    )
 
 
 if __name__ == "__main__":
